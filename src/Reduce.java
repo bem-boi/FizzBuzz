d219f6602f76fd7e530098f07ebc5e094f3e8272
beam <108953955+bem-boi@users.noreply.github.com>
2024-09-09 18:09:30 -0400

    completed Reduce.java

---
+++ src/Reduce.java
@@ -0,0 +1,18 @@
+public class Reduce {
+    public static void main(String[] args) {
+        System.out.println(reduce(100));
+    }
+
+    private static int reduce(int n){
+        int steps = 0;
+        while (n > 0){
+            if (n % 2 == 0){
+                n /= 2;
+            }else {
+                n -= 1;
+            }
+            steps += 1;
+        }
+        return steps;
+    }
+}
